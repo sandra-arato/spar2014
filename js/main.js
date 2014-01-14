bca92caa98b26b0378406ec0e6e89ba38736c663
Sandra Szenti <alexandraszenti@gmail.com>
2014-01-14 15:16:13 -0500

    styling and google map added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,80 @@
+function firstMapLoad (currentPlace) {
+
+	var MY_MAPTYPE_ID = 'custom_style';
+	var featureOpts = [
+		{
+			"featureType": "water",
+			"stylers": [
+				{ "hue": "#00ffcc" },
+				{ "saturation": -47 },
+				{ "lightness": -24}
+			]
+		},{
+			"featureType": "landscape",
+			"stylers": [
+				{ "hue": "#4cff00" },
+				{ "lightness": -33},
+				{ "gamma": 1.04 }
+			]
+		},{
+			"featureType": "road",
+			"stylers": [
+				{ "hue": "#ffa200" },
+				{ "weight": 0.4 }
+			]
+		},{
+			"featureType": "poi",
+			"elementType": "labels",
+			"stylers": [
+				{ "hue": "#00ff6f" },
+				{ "lightness": -28 }
+			]
+		},{
+			"featureType": "administrative",
+			"stylers": [
+				{ "hue": "#0077ff" },
+				{ "lightness": -20 },
+				{ "gamma": 1.33 }
+			]
+		}
+	]
+	
+	var styledMapOptions = {
+		name: 'Custom Style'
+	};
+
+	var mapOptions = {
+		zoom: 1,
+		center: new google.maps.LatLng(currentPlace[0],currentPlace[1]),
+		panControl: false,
+		zoomControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		streetViewControl: false,
+		overviewMapControl: false,
+		disableDoubleClickZoom: false,
+		draggable: true,
+		scrollwheel: true,
+		mapTypeControlOptions: {
+			mapTypeIds: [
+				google.maps.MapTypeId.ROADMAP,
+				MY_MAPTYPE_ID
+			]
+		},
+		mapTypeId: MY_MAPTYPE_ID
+	};
+
+	var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+	placeMarkers();
+}
+
 
 function initialize() {
-	
+	var placeInit = [38.79555,-104.84136];
+	firstMapLoad(placeInit);
 	
 }
 
